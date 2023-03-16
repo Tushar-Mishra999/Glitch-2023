ec420fe552c90483be4699968ea455c7015de7fe
Tushar-Mishra999 <75738710+Tushar-Mishra999@users.noreply.github.com>
2023-03-16 19:35:47 +0530

    auth fixes

--- lib/views/registration_screen.dart
+++ lib/views/registration_screen.dart
@@ -30,97 +30,95 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: kBgColor,
-      body: Form(
-        key: _formKey,
-        child: Center(
-          child: SingleChildScrollView(
-            child: Stack(
-              children: [
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: size.height * 0.05,
-                      ),
-                      CustomTextField(
-                        title: "Name",
-                        controller: nameController,
-                        hintText: "Enter your name",
-                      ),
-                      CustomTextField(
-                        title: "Email",
-                        controller: emailController,
-                        hintText: "Enter your email",
-                      ),
-                      CustomTextField(
-                        title: "Password",
-                        controller: passwordController,
-                        hintText: "Enter your password",
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      RoundedButton(
-                        size: size,
-                        title: 'Register',
-                        func: () {
-                          isLoaded = true;
-                          setState(() {});
-                          AuthService authService = AuthService();
-                          authService.signUpUser(
-                              context: context,
-                              name: nameController.text,
-                              email: emailController.text,
-                              password: passwordController.text);
-                          isLoaded = false;
-                          setState(() {});
-                        },
-                        second: false,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LoginScreen()));
-                          },
-                          child: RichText(
-                            textAlign: TextAlign.center,
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: "Already have an account? ",
-                                  style: GoogleFonts.sourceSansPro(
-                                    color: color1,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: "Login",
-                                  style: GoogleFonts.sourceSansPro(
-                                    color: color1,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              ],
-                            ),
+      resizeToAvoidBottomInset: false,
+      body: Stack(children: [
+        Center(
+          child: Form(
+            key: _formKey,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              CustomTextField(
+                title: "Name",
+                controller: nameController,
+                hintText: "Enter your name",
+              ),
+              CustomTextField(
+                title: "Email",
+                controller: emailController,
+                hintText: "Enter your email",
+              ),
+              CustomTextField(
+                title: "Password",
+                obscure: true,
+                controller: passwordController,
+                hintText: "Enter your password",
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              RoundedButton(
+                size: size,
+                title: 'Register',
+                func: () async {
+                  isLoaded = true;
+                  setState(() {});
+                  AuthService authService = AuthService();
+                  await authService.signUpUser(
+                      context: context,
+                      name: nameController.text,
+                      email: emailController.text,
+                      password: passwordController.text);
+                  isLoaded = false;
+                  setState(() {});
+                },
+                second: false,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Already have an account? ",
+                          style: GoogleFonts.sourceSansPro(
+                            color: color1,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
-                      ),
-                    ]),
-                    isLoaded?const CircularProgressIndicator(
-                      color: color1,
-                    ):Container()
-              ],
-            ),
+                        TextSpan(
+                          text: "Login",
+                          style: GoogleFonts.sourceSansPro(
+                            color: color1,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ]),
           ),
         ),
-      ),
+        isLoaded
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: color1,
+                ),
+              )
+            : Container(),
+      ]),
     );
   }
 }
